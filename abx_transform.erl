a8fe2f5c6349fa2c32f35b62c73db5c0cd2e5c3c
András Veres-Szentkirályi <vsza@vsza.hu>
2015-01-12 16:21:21 +0100

    added transformation parsing

--- abx_transform.erl
+++ abx_transform.erl
@@ -1,6 +1,24 @@
 -module(abx_transform).
 -export([apply_transformation/2]).
 
+-define(TRANSFORM_RE, "^([a-z]+)\\\.([a-z]+)=(.+)$").
+
 apply_transformation(Transformation, Model) ->
-	io:format(standard_error, "TODO ~p~n", [Transformation]),
+	io:format(standard_error, "TODO ~p~n", [compile_transformation(Transformation)]),
 	Model.
+
+compile_transformation(Transformation) ->
+	case re:run(Transformation, ?TRANSFORM_RE,
+			[anchored, {capture, all_but_first, binary}, caseless]) of
+		{match, [Element, Attribute, Value]} ->
+			{Element, Attribute, compile_value(Value)};
+		nomatch -> throw({invalid_transformation, Transformation})
+	end.
+
+compile_value(<<"true">>) -> true;
+compile_value(<<"false">>) -> false;
+compile_value(String) ->
+	case lists:all(fun (C) -> C =< $9 andalso C >= $0 end, binary_to_list(String)) of
+		true -> binary_to_integer(String);
+		false -> String
+	end.

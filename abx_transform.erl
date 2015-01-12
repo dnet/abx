d1b91810c94e4826f7c5a24ad96473f3a578acbc
András Veres-Szentkirályi <vsza@vsza.hu>
2015-01-12 16:41:17 +0100

    update string pool to include transform values

--- abx_transform.erl
+++ abx_transform.erl
@@ -4,8 +4,20 @@
 -define(TRANSFORM_RE, "^([a-z]+)\\\.([a-z]+)=(.+)$").
 
 apply_transformation(Transformation, Model) ->
-	io:format(standard_error, "TODO ~p~n", [compile_transformation(Transformation)]),
-	Model.
+	StringPool = proplists:get_value(string_pool, Model),
+	CompiledTF = compile_transformation(Transformation),
+	ExtendedStringPool = extend_stringpool(StringPool, CompiledTF),
+	io:format(standard_error, "TODO ~p~n", [CompiledTF]),
+	[{string_pool, ExtendedStringPool} | proplists:delete(string_pool, Model)].
+
+extend_stringpool(Pool, Params) when is_tuple(Params) ->
+	extend_stringpool(Pool, lists:filter(fun erlang:is_binary/1, tuple_to_list(Params)));
+extend_stringpool(Pool, []) -> Pool;
+extend_stringpool(Pool, [Param | Params]) ->
+	case lists:member(Param, Pool) of
+		true -> extend_stringpool(Pool, Params);
+		false -> extend_stringpool(Pool ++ [Param], Params)
+	end.
 
 compile_transformation(Transformation) ->
 	case re:run(Transformation, ?TRANSFORM_RE,
